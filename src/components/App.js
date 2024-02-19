cdd08ac0e161d33497b8da6875e812af8014ec10
Nii Derry <derricktagoe@gmail.com>
2024-02-19 20:43:54 +0000

    added the sort list

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Logo from "./Logo.js";
+import Form from "./Form.js";
+import PackingList from "./PackingList.js";
+import Stats from "./Stats.js";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    // updating an array
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList(items) {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    // setItems((items) => items.splice(0, items.length));
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+
+      <Form onAddItems={handleAddItems} />
+
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearItems={handleClearList}
+      />
+
+      <Stats items={items} />
+    </div>
+  );
+}
